66960d08c0a92102a917f1eb02302489e2db8d36
anon <sarah.anne.friend@gmail.com>
2019-03-18 12:56:59 -0400

    Fetching polls

--- client/src/index.js
+++ client/src/index.js
@@ -1,12 +1,39 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import * as serviceWorker from './serviceWorker';
+import React, { Component } from 'react'
+import ReactDOM from 'react-dom'
+import './index.css'
+import { apiUrl } from './env'
 
-ReactDOM.render(<App />, document.getElementById('root'));
+class App extends Component {
 
-// If you want your app to work offline and load faster, you can change
-// unregister() to register() below. Note this comes with some pitfalls.
-// Learn more about service workers: https://bit.ly/CRA-PWA
-serviceWorker.unregister();
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      data: null,
+    };
+  }
+
+  componentDidMount() {
+    return fetch(`${apiUrl}polls`, {
+	  method: "GET",
+	  headers: {
+	    "Accept": "text/html,application/xhtml+xml,application/xml;q=0.9,*/*;q=0.8"
+	  }})
+      .then(response => {
+      	console.log(response)
+      	return response.json()
+      })
+      .then(data => this.setState({ data }));
+  }
+
+  render() {
+  	console.log(this.state.data)
+    return (
+      <div className="App">
+        <h1>{apiUrl}!</h1>
+      </div>
+    )
+  }
+}
+
+ReactDOM.render(<App />, document.getElementById('root'))
