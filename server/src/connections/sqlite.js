c5c4224bdd7932b24fa2b67b60141a5c8601d23b
anon <sarah.anne.friend@gmail.com>
2019-03-21 20:39:58 -0400

    If not exists

--- server/src/connections/sqlite.js
+++ server/src/connections/sqlite.js
@@ -5,15 +5,15 @@ const db = new sqlite3.Database('./quadratic.db', (err) => {
   if (err) {
     return console.error(err.message);
   }
-  console.log('Connected to the in-memory sqlite database.');
+  console.log('Connected to sqlite database.');
 });
  
 db.serialize(async () => {
-  db.run("CREATE TABLE users (id INTEGER PRIMARY KEY ASC, username TEXT, password TEXT)");
-  db.run("CREATE TABLE polls (id INTEGER PRIMARY KEY ASC, question TEXT)");
-  db.run("CREATE TABLE pollItems (id INTEGER PRIMARY KEY ASC, answer TEXT, pollsId INT, FOREIGN KEY(pollsId) REFERENCES polls(id))");
-  db.run("CREATE TABLE hasVoted (id INTEGER PRIMARY KEY ASC, sessionId TEXT, pollsId INT, FOREIGN KEY(pollsId) REFERENCES polls(id))");
-  db.run("CREATE TABLE votes " +
+  db.run("CREATE TABLE IF NOT EXISTS users (id INTEGER PRIMARY KEY ASC, username TEXT, password TEXT)");
+  db.run("CREATE TABLE IF NOT EXISTS polls (id INTEGER PRIMARY KEY ASC, question TEXT)");
+  db.run("CREATE TABLE IF NOT EXISTS pollItems (id INTEGER PRIMARY KEY ASC, answer TEXT, pollsId INT, FOREIGN KEY(pollsId) REFERENCES polls(id))");
+  db.run("CREATE TABLE IF NOT EXISTS hasVoted (id INTEGER PRIMARY KEY ASC, sessionId TEXT, pollsId INT, FOREIGN KEY(pollsId) REFERENCES polls(id))");
+  db.run("CREATE TABLE IF NOT EXISTS votes " +
   	"(id INTEGER PRIMARY KEY ASC, voteWeight INT, pollsId INT, pollItemsId INT," +
   	"FOREIGN KEY(pollsId) REFERENCES polls(id), FOREIGN KEY(pollItemsId) REFERENCES pollItems(id))");
 });
